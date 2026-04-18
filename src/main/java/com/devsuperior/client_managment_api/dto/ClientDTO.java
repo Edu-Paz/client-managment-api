98e18ced311b5372d705e59ff4fe0e847e09cc98
Edu-Paz <eduardopazcarv@gmail.com>
2026-04-18 13:25:08 -0300

    feat: add ClientService and ClientDTO

---
+++ src/main/java/com/devsuperior/client_managment_api/dto/ClientDTO.java
@@ -0,0 +1,65 @@
+package com.devsuperior.client_managment_api.dto;
+
+import com.devsuperior.client_managment_api.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+    private Long id;
+    @NotBlank(message = "Required field")
+    private String name;
+    private String cpf;
+    @Positive(message = "Income must be positive")
+    private Double income;
+    @PastOrPresent(message = "The date must be in the past or present")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client entity){
+        id = entity.getId();
+        name = entity.getName();
+        cpf = entity.getCpf();
+        income = entity.getIncome();
+        birthDate = entity.getBirthDate();
+        children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
---
+++ src/main/java/com/devsuperior/client_managment_api/services/ClientService.java
@@ -0,0 +1,77 @@
+package com.devsuperior.client_managment_api.services;
+
+import com.devsuperior.client_managment_api.dto.ClientDTO;
+import com.devsuperior.client_managment_api.entities.Client;
+import com.devsuperior.client_managment_api.repositories.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
+
+
+public class ClientService {
+    @Autowired
+    private ClientRepository repository;
+
+    // Insert a new client
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    // Find a client by id
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow(() -> new RuntimeException());
+
+        return new ClientDTO(client);
+    }
+
+    // Find all clients
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+
+        return result.map(x -> new ClientDTO(x));
+    }
+
+    // Update a client
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException(("Entity not found."));
+        }
+    }
+
+    // Delete a client
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException(("Entity not found"));
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Referential integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
+}
