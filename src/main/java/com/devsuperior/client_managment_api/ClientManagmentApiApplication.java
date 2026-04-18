abd24790aef6645ff5380359c3acc49a8e2c8549
Edu-Paz <eduardopazcarv@gmail.com>
2026-04-18 10:43:05 -0300

    First commit

---
+++ src/main/java/com/devsuperior/client_managment_api/ClientManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.client_managment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/client_managment_api/ClientManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.client_managment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
